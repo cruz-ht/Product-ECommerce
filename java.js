e8de1f013759b818808cb8cbf465fbc37768896b
cruz-ht <jessicacruz793@yahoo.com>
2025-11-25 20:41:52 -0500

    COMPLETE json product file

--- java.js
+++ java.js
@@ -3,6 +3,37 @@
 const hamburger = document.querySelector(".hamburger-Menu")
         const navLinks = document.querySelector(".navbar-Menu")
  
-        hamburger.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
+        hamburger.addEventListener("click",()=>{
+        navLinks.classList.toggle("mobile-menu")
         })
+
+
+// JSON FILE 
+
+// assigning the Var to the corresponding contianer
+
+const container = document.querySelector(".grid-Container")
+
+
+fetch("products.json")
+
+  .then(response => response.json())
+
+
+  .then(products => {
+
+        
+        products.forEach(product => {
+        container.innerHTML += `
+        <main class="product-card">
+          <img src="${product.image}" alt="${product.alt}">
+          <h3>${product.name}</h3>
+          <p>${product.description}</p>
+          <p>$${product.price}</p>
+        </main>`;
+    });
+
+  })
+
+
+  .catch(error => console.error("Error loading JSON:", error));
